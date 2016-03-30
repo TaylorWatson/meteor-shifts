b4219ebd9e1827e575393ab07fc1a2be557923dc
Andrew Hartwig <andrew.hartwig1@gmail.com>
2016-03-30 12:10:55 -0400

    Initial commit of mshifts application

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <h1 className="ui dividing header">Hello world!</h1>
+      </div>
+    );
+  }
+}
